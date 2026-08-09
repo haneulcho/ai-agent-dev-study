1cb082b25de029ed6ad20b9fe0434087a0b4d50f
Haneul Cho <stuykr@naver.com>
2026-08-09 20:13:27 +0900

    init: 샘플 프로젝트 추가

---
+++ examples/projects-plli/main.py
@@ -0,0 +1,8 @@
+x = {"a": 37, "b": 42, "c": 927}
+
+
+def very_long_function_name(arg1, arg2, arg3, arg4="hello", arg5=True):
+    return arg1 + arg2
+
+
+y = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
